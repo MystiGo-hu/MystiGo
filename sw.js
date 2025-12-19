e29b45a8530c7a9613a68b25b91104c0e6000afa
MystiGo-hu <mystigo.net@gmail.com>
2025-12-19 12:24:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const CACHE_NAME = 'mystigo-cache-v1.7';
 const STATIC_ASSETS = [
     './index2.html',
     './manifest.json',
-    './logo/icon-512_v4.png'
-    './logo/icon-192_v3.png',
+    './logo/icon-512_v4.png', // Vessző pótolva! (Splashhez kell)
+    './logo/icon-192_v3.png', // Ez is kell (Ikonhoz)
 ];
 
 self.addEventListener('install', (event) => {
